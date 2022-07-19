b61e40ec322ff314b846c68df417041c4b68e05c
erik-jj <erik.juarezjuarez@gmail.com>
2022-07-18 19:03:18 -0600

    carrito de compras finalizado

--- src/containers/ProductDetail.jsx
+++ src/containers/ProductDetail.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ProductInfo from '../components/ProductInfo';
 import '../styles/ProductDetail.scss';
+import closeImage from '../assets/icons/icon_close.png'
 
 const ProductDetail = () => {
 	return (
 		<aside className="ProductDetail">
 			<div className="ProductDetail-close">
-				<img src="./icons/icon_close.png" alt="close" />
+				<img src={closeImage} alt="close" />
 			</div>
 			<ProductInfo />
 		</aside>
